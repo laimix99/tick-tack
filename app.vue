a19b46125a8586d69c9bb0fb2210ee0c5df82bc7
laimix99 <80690937+laimix99@users.noreply.github.com>
2023-01-24 16:18:17 +0300

    2 files modified

--- app.vue
+++ app.vue
@@ -1,145 +1,79 @@
 <script setup>
-// const state = reactive({
-//   player: 'X',
-//   squares: [
-//     ['', '', ''],
-//     ['', '', ''],
-//     ['', '', ''],
-//   ],
-// winner: computed(() => calculateWinner(state.squares.flat()))
-// });
-const player = ref('X');
-const squares = ref([
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-]);
-const winner = computed(() => calculateWinner(squares.value.flat()));
+// import { ref, computed } from 'vue'
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 5],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+const player = ref('X')
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+])
+
+const CalculateWinner = (board) => {
+  const lines = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    const [a, b, c] = lines[i]
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a]
     }
   }
-  return null;
+
+  return null
 }
 
-function move(x, y) {
-  if (winner.value) return;
-  squares.value[x][y] = player.value;
-  player.value = player.value === 'O' ? 'X' : 'O';
+const winner = computed(() => CalculateWinner(board.value.flat()))
+
+const MakeMove = (x, y) => {
+	if (winner.value) return
+
+	if (board.value[x][y]) return
+
+	board.value[x][y] = player.value
+
+	player.value = player.value === 'X' ? 'O' : 'X'
 }
 
-function reset() {
-  player.value = 'X';
-  squares.value = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
-  ];
+const ResetGame = () => {
+	board.value = [
+		['', '', ''],
+		['', '', ''],
+		['', '', '']
+	]
+	player.value = 'X'
 }
 
-onMounted(() => {});
 </script>
 
 <template>
-  <div class="w-full flex flex-col items-center justify-center gap-6">
-    <div class="w-full flex flex-col items-center justify-center gap-6">
-      <h1 v-if="winner">winner {{ winner }}</h1>
-      <h2 v-else>{{ player }}</h2>
-      <button @click="reset">reset</button>
-    </div>
-    <div class="flex flex-col flex-wrap items-center w-full cursor-pointer">
-      <div
-        v-for="x in 3"
-        :key="x"
-        class="
-          <!--
-          cub
-          flex flex-row
-          justify-center
-          items-center
-          hover:bg-red
-          bb
-          -->
-        "
-      >
-        <button
-          v-for="y in 3"
-          :key="y"
-          @click="move(x, y)"
-          class="w-50px h-50px"
-        >
-          {{ squares[x][y] }}
-        </button>
-      </div>
-    </div>
-  </div>
+	<main class="pt-8 text-center">
+		<h3 class="text-xl mb-4">Игрок: {{ player }}</h3>
+
+		<div class="flex flex-col items-center mb-8">
+			<div 
+				v-for="(row, x) in board" 
+				:key="x"
+				class="flex">
+				<div 
+					v-for="(cell, y) in row" 
+					:key="y" 
+					@click="MakeMove(x, y)" 
+					class=" br w-24 h-24  flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ">
+					{{ board[x][y] }}
+				</div>
+			</div>
+		</div>
+
+		<div class="text-center">
+			<h2 v-if="winner" class="">Игрок '{{ winner }} победил</h2>
+			<button @click="ResetGame">Reset</button>
+		</div>
+	</main>
 </template>
 
 <style>
-/* .br {
-  outline: 1px solid red;
-}
-.cub {
-}
-.cub:nth-child(1) {
-  border-right: 2px solid black;
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(4) {
-  border-right: 2px solid black;
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(2) {
-  border-right: 2px solid black;
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(5) {
-  border-right: 2px solid black;
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(7) {
-  border-right: 2px solid black;
-}
-.cub:nth-child(3) {
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(6) {
-  border-bottom: 2px solid black;
-}
-.cub:nth-child(9) {
-  border-left: 2px solid black;
-}
 
-.cell {
-  width: 50px;
-  height: 50px;
-  background-color: white;
-  border: black 1px double;
-  cursor: pointer;
-  display: inline-block;
-  user-select: none;
+.br {
+  outline: 1px solid black;
 }
-
-.cell:hover {
-  border: 1px double red;
-}
-
-.X,
-.O {
-  font-size: 40px;
-} */
 </style>
