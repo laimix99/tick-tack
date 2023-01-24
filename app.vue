91fd60ffe4c5821c524301c68ec78960a9f9044e
laimix99 <80690937+laimix99@users.noreply.github.com>
2023-01-24 17:12:56 +0300

    1 file modified

--- app.vue
+++ app.vue
@@ -9,7 +9,16 @@ const board = ref([
 ])
 
 const CalculateWinner = (board) => {
-  const lines = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
